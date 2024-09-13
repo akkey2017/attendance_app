d2eba10b768d4ab5f44a3d446a228695fb0851a1
akkey2017 <a23.jkbd@g.chuo-u.ac.jp>
2024-09-13 10:53:17 +0900

    delete unused variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
-import Image from "next/image";
 import localFont from "next/font/local";
 import { useState, useEffect } from 'react';  
 import axios from 'axios';  
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 type Attendance = {  
   userId: string;  
