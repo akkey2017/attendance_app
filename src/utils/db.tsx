e27c8666b87b59af5cfba178afdbc2bedb57b484
akkey2017 <a23.jkbd@g.chuo-u.ac.jp>
2024-09-13 10:42:15 +0900

    add db connecter

---
+++ src/utils/db.tsx
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+export default connectToDatabase;
