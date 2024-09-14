159b095a6a3262a0d0aff4c29901c1cebb0ab003
akkey2017 <a23.jkbd@g.chuo-u.ac.jp>
2024-09-15 05:12:40 +0900

    cron helper update

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -7,6 +7,7 @@ const deleteExpiredAttendances = async (db: any) => {
     console.log('Deleting attendances before: ', pastWeekDate);
     try {  
         await db.collection('attendances').deleteMany({ date: { $lt: pastWeekDate } });  
+        await db.collection('meeting-time').deleteMany({ date: { $lt: pastWeekDate } });
         console.log('Expired attendances deleted');  
     } catch (error) {  
         console.error('Failed to delete expired attendances:', error);  
