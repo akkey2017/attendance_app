e05b221fb115e185bc8d50614ae21dc3f2a8f9d0
akkey2017 <a23.jkbd@g.chuo-u.ac.jp>
2024-09-15 00:59:36 +0900

    update mongo client

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -9,18 +9,9 @@ const uri = process.env.MONGODB_URI;
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-    // In development mode, use a global variable so connection is cached  
-    if (!global._mongoClientPromise) {
-        client = new MongoClient(uri);
-        global._mongoClientPromise = client.connect();
-    }
-    clientPromise = global._mongoClientPromise;
-} else {
-    // In production mode, it's best to not use a global variable  
-    client = new MongoClient(uri);
-    clientPromise = client.connect();
-}
+
+client = new MongoClient(uri);
+clientPromise = client.connect();
 
 export async function connectToDatabase() {
     const client = await clientPromise;
