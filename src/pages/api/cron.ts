094b3b4bf5c0be1a56bb29268827c2aa066490e0
akkey2017 <a23.jkbd@g.chuo-u.ac.jp>
2024-09-25 11:43:27 +0900

    Refactor cron job and room creation API endpoint

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';  
 import { connectToDatabase } from '../../lib/mongodb';  
 import { getPastDate } from '../../utils/dateHelpers';  
+import { Db } from 'mongodb';
 
-const deleteExpiredAttendances = async (db: any) => {  
+const deleteExpiredAttendances = async (db: Db) => {  
     const pastWeekDate = getPastDate(0);
     console.log('Deleting attendances before: ', pastWeekDate);
     try {  
@@ -14,6 +15,40 @@ const deleteExpiredAttendances = async (db: any) => {
     }  
 };  
 
+const autoSetupMeetingTime = async (db: Db) => {
+    const meetingWeekDay = ["Wednesday", "Friday"];
+    const timeStartsMeeting = ["22:00","22:00"];
+    const meetingCounts = [4,4];
+
+    try {
+        const today = new Date();
+        for (let i = 0; i < 7; i++) {
+            const checkDate = new Date(today);
+            checkDate.setDate(today.getDate() + i);
+            const dayOfWeek = checkDate.toLocaleString('en-US', { weekday: 'long' });
+
+            const index = meetingWeekDay.indexOf(dayOfWeek);
+            if (index !== -1) {
+                const meetingTime = timeStartsMeeting[index];
+                const meetingCount = meetingCounts[index];
+
+                const existingMeeting = await db.collection('meeting-time').findOne({ date: checkDate });
+                if (!existingMeeting) {
+                    await db.collection('meeting-time').insertOne({
+                        userId: "cron-job",
+                        date: checkDate,
+                        startTime: meetingTime,
+                        repeartCount: meetingCount
+                    });
+                    console.log(`Meeting time set for ${dayOfWeek} at ${meetingTime}`);
+                }
+            }
+        }
+    } catch (error) {
+        console.error('Failed to auto setup meeting time:', error);
+    }
+};
+
 export default async function (  
     req: NextApiRequest,  
     res: NextApiResponse  
@@ -21,6 +56,7 @@ export default async function (
     const { db } = await connectToDatabase();  
 
     await deleteExpiredAttendances(db);  
+    await autoSetupMeetingTime(db);
 
     res.status(200).end(); // レスポンスを終了  
 }
